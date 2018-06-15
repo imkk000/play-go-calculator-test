adb14022e2dcd1cd623a840c6b6f7ae536a565b0
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-06-15 19:39:41 +0700

    Added test and production file of Add function

---
+++ calculator.go
@@ -0,0 +1,5 @@
+package calculator
+
+func Add(number1, number2 int) int {
+	return number1 + number2
+}
--- calculator_test.go
+++ calculator_test.go
@@ -2,3 +2,24 @@ package calculator
 
 import "testing"
 
+func Test_Add_Input_Minus_1_Plus_1_Should_Be_0(t *testing.T) {
+	number1 := -1
+	number2 := 1
+	expected := 0
+	resultNumber := Add(number1, number2)
+	isEqual(t, expected, resultNumber)
+}
+
+func Test_Add_Input_1_plus_1_Should_Be_2(t *testing.T) {
+	number1 := 1
+	number2 := 1
+	expected := 2
+	resultNumber := Add(number1, number2)
+	isEqual(t, expected, resultNumber)
+}
+
+func isEqual(t *testing.T, expected, actual int) {
+	if actual != expected {
+		t.Errorf("expected %d, but %d\n", expected, actual)
+	}
+}
